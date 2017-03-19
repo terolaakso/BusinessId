4cd5406aa70c6f06863ffdbf2a80453196fc5ed0
Tero <Tero@ASUS2015>
2017-03-19 12:45:18 +0200

    Add project files.

---
+++ BusinessId/BusinessIdSpecification.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessId
+{
+    /// <summary>
+    /// Determines if the given string is a valid business id.
+    /// If not, reasons are available from ReasonsForDissatisfaction property.
+    /// Business id validity rules are defined at http://www.finlex.fi/fi/laki/ajantasa/2001/20010288#P3
+    /// </summary>
+    public class BusinessIdSpecification : ISpecification<string>
+    {
+        private static readonly int[] checksumMultipliers = new int[] { 2, 4, 8, 5, 10, 9, 7 };
+        private List<string> reasons = new List<string>();
+
+        public IEnumerable<string> ReasonsForDissatisfaction
+        {
+            get
+            {
+                return reasons;
+            }
+        }
+
+        public bool IsSatisfiedBy(string entity)
+        {
+            reasons.Clear();
+
+            if (IsNull(entity))
+            {
+                return false;
+                // no more checks possible
+            }
+
+            // non short-circuiting &-operator, because we want to get as many reasons as possible and not to stop at first reason
+            var result = HasCorrectLength(entity) & 
+                HasValidSeparator(entity) & 
+                HasValidDigits(entity) &
+                HasValidChecksum(entity);
+
+            return result;
+        }
+
+        private bool IsNull(string businessId)
+        {
+            if (businessId == null)
+            {
+                reasons.Add("BusinessId should not be null");
+                return true;
+            }
+            return false;
+        }
+
+        private bool HasCorrectLength(string businessId)
+        {
+            var result = businessId.Length == 9;
+            if (!result)
+            {
+                reasons.Add("BusinessId length is not 9 characters");
+            }
+            return result;
+        }
+
+        private bool HasValidSeparator(string businessId)
+        {
+            var separatorIndexPos = businessId.Length - 2;
+            if (separatorIndexPos < 0)
+            {
+                // business id too short, it's reported in IsCorrectLength
+                return false;
+            }
+            var isValidSeparator = businessId[separatorIndexPos] == '-';
+            if (!isValidSeparator)
+            {
+                reasons.Add("Separator character '-' not found in correct position");
+            }
+            return isValidSeparator;
+        }
+
+        private bool HasValidDigits(string businessId)
+        {
+            var result = true;
+            for (int i = 0; i < businessId.Length; i++)
+            {
+                if (i != businessId.Length - 2) // do not check the separator position
+                {
+                    var isDigit = char.IsDigit(businessId, i);
+                    if (!isDigit)
+                    {
+                        reasons.Add(string.Format(CultureInfo.CurrentCulture, "Character {0} at position {1} is not a digit", businessId[i], i + 1));
+                    }
+                    result &= isDigit;
+                }
+            }
+            return result;
+        }
+
+        private static int CalculateChecksumRemainder(string businessId)
+        {
+            var sum = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                int digit;
+                var index = businessId.Length - 3 - i;
+                if (index >= 0)
+                {
+                    if (int.TryParse(businessId[index].ToString(), out digit))
+                    {
+                        sum += digit * checksumMultipliers[i];
+                    }
+                }
+            }
+            return sum % 11;
+        }
+
+        private bool HasValidChecksum(string businessId)
+        {
+            int actualChecksum;
+            if (!string.IsNullOrEmpty(businessId) && int.TryParse(businessId[businessId.Length - 1].ToString(), out actualChecksum))
+            {
+                var result = true;
+                if (actualChecksum == 1)
+                {
+                    reasons.Add("Checksum cannot be 1");
+                    result = false;
+                }
+                var checksumRemainder = CalculateChecksumRemainder(businessId);
+                var calculatedChecksum = checksumRemainder == 0 ? checksumRemainder : 11 - checksumRemainder;
+                if (actualChecksum != calculatedChecksum)
+                {
+                    reasons.Add(string.Format(CultureInfo.CurrentCulture, "Checksum {0} is not correct, should be {1}", actualChecksum, calculatedChecksum));
+                    result = false;
+                }
+                return result;
+            }
+            // checksum is not a digit
+            return false;
+        }
+    }
+}
---
+++ BusinessIdTest/BusinessIdSpecificationTest.cs
@@ -0,0 +1,152 @@
+﻿using BusinessId;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace BusinessIdTest
+{
+    [TestClass]
+    public class BusinessIdSpecificationTest
+    {
+        [TestMethod]
+        public void NullTest()
+        {
+            string businessId = null;
+            var spec = new BusinessIdSpecification();
+            var actual = spec.IsSatisfiedBy(businessId);
+            Assert.IsFalse(actual);
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("BusinessId should not be null"));
+        }
+
+        [TestMethod]
+        public void LengthTest()
+        {
+            // Too short
+            var businessId = "12345678";
+            var spec = new BusinessIdSpecification();
+            var actual = spec.IsSatisfiedBy(businessId);
+            Assert.IsFalse(actual);
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("BusinessId length is not 9 characters"));
+
+            // Too long
+            businessId = "1234567890";
+            actual = spec.IsSatisfiedBy(businessId);
+            Assert.IsFalse(actual);
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("BusinessId length is not 9 characters"));
+
+            // Empty
+            businessId = string.Empty;
+            actual = spec.IsSatisfiedBy(businessId);
+            Assert.IsFalse(actual);
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("BusinessId length is not 9 characters"));
+
+            // Correct length
+            businessId = "123456789";
+            actual = spec.IsSatisfiedBy(businessId);
+            Assert.IsFalse(spec.ReasonsForDissatisfaction.Contains("BusinessId length is not 9 characters"));
+        }
+
+        [TestMethod]
+        public void SeparatorTest()
+        {
+            // Wrong separator
+            var businessId = "1234567.8";
+            var spec = new BusinessIdSpecification();
+            var actual = spec.IsSatisfiedBy(businessId);
+            Assert.IsFalse(actual);
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("Separator character '-' not found in correct position"));
+
+            // Wrong position
+            businessId = "-------0-";
+            actual = spec.IsSatisfiedBy(businessId);
+            Assert.IsFalse(actual);
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("Separator character '-' not found in correct position"));
+
+            // Correct separator in correct position
+            businessId = "---------";
+            actual = spec.IsSatisfiedBy(businessId);
+            Assert.IsFalse(spec.ReasonsForDissatisfaction.Contains("Separator character '-' not found in correct position"));
+        }
+
+        [TestMethod]
+        public void DigitTest()
+        {
+            // No digits
+            var businessId = "ABCDEFGH½";
+            var spec = new BusinessIdSpecification();
+            var actual = spec.IsSatisfiedBy(businessId);
+            Assert.IsFalse(actual);
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("Character A at position 1 is not a digit"));
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("Character B at position 2 is not a digit"));
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("Character C at position 3 is not a digit"));
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("Character D at position 4 is not a digit"));
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("Character E at position 5 is not a digit"));
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("Character F at position 6 is not a digit"));
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("Character G at position 7 is not a digit"));
+            Assert.IsFalse(spec.ReasonsForDissatisfaction.Contains("Character H at position 8 is not a digit"));
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("Character ½ at position 9 is not a digit"));
+
+            // Correct digits
+            businessId = "1234567A8";
+            actual = spec.IsSatisfiedBy(businessId);
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.All(r => !r.EndsWith("is not a digit", StringComparison.CurrentCulture)));
+        }
+
+        [TestMethod]
+        public void ChecksumTest()
+        {
+            var businessId = "1234567-1";
+            var spec = new BusinessIdSpecification();
+            var actual = spec.IsSatisfiedBy(businessId);
+            Assert.IsFalse(actual);
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("Checksum cannot be 1"));
+
+            businessId = "0204819-9";
+            actual = spec.IsSatisfiedBy(businessId);
+            Assert.IsFalse(actual);
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("Checksum 9 is not correct, should be 8"));
+        }
+
+        [TestMethod]
+        public void CorrectBusinessIdTest()
+        {
+            var businessId = "0204819-8";
+            var spec = new BusinessIdSpecification();
+            var actual = spec.IsSatisfiedBy(businessId);
+            Assert.IsTrue(actual);
+            Assert.IsFalse(spec.ReasonsForDissatisfaction.Any());
+
+            businessId = "2542362-4";
+            actual = spec.IsSatisfiedBy(businessId);
+            Assert.IsTrue(actual);
+            Assert.IsFalse(spec.ReasonsForDissatisfaction.Any());
+        }
+
+        [TestMethod]
+        public void MultipleReasonsTest()
+        {
+            var businessId = "A20481.1";
+            var spec = new BusinessIdSpecification();
+            var actual = spec.IsSatisfiedBy(businessId);
+            Assert.IsFalse(actual);
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("BusinessId length is not 9 characters"));
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("Separator character '-' not found in correct position"));
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("Character A at position 1 is not a digit"));
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("Checksum cannot be 1"));
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Contains("Checksum 1 is not correct, should be 2"));
+        }
+
+        [TestMethod]
+        public void ReasonsGetClearedTest()
+        {
+            var businessId = "A20481.1"; // invalid business id
+            var spec = new BusinessIdSpecification();
+            spec.IsSatisfiedBy(businessId);
+            Assert.IsTrue(spec.ReasonsForDissatisfaction.Any());
+
+            businessId = "0204819-8"; // valid business id
+            spec.IsSatisfiedBy(businessId); // same spec object
+            Assert.IsFalse(spec.ReasonsForDissatisfaction.Any());
+        }
+    }
+}
